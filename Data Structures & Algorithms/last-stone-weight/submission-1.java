db3b0edff4af61e91003cf7ea26cebd743be8a53
Poornima <47754043+POORNIMAMOGAVEERA@users.noreply.github.com>
2026-06-16 01:07:48 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b)->b -a);
+
+        for(int stone: stones){
+            heap.offer(stone);
+        }
+        while(heap.size()>1){
+            int a= heap.poll();
+            int b= heap.poll();
+            if(a>b){
+                heap.offer(a-b);
+            }
+        }
+        return heap.isEmpty()?0:heap.peek(); 
+    }
+}
