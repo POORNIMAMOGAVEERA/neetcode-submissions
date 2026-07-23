0319bef81ed82128a30323b1047217e1c57de497
Poornima <47754043+POORNIMAMOGAVEERA@users.noreply.github.com>
2026-07-23 11:26:55 +0530

    Add: time-needed-to-buy-tickets - submission-3

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count =0;
+        for(int i=0;i<tickets.length;i++){
+            if(i>k && tickets[i]>=tickets[k]){
+              count+= (tickets[k]-1);
+            }else if(tickets[i] < tickets[k]){
+              count+=tickets[i];
+            }else{
+              count += tickets[k];
+            }
+        }
+        return count;
+    }
+}
